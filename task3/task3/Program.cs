7db3f7f760d5b8ca328b87e9ea9a57cac6c690dd
Denis Vladimirov <4rx311@gmail.com>
2018-11-26 15:00:41 +0300

    task 3 init

---
+++ task3/task3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace InputOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory direct = new Directory();
+        }
+    }
+}
