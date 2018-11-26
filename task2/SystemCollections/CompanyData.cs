884e4e55f3e674d6f65b2f7dcf8701f31632b4fb
Denis Vladimirov <4rx311@gmail.com>
2018-11-26 10:50:21 +0300

    task2 - hash table

---
+++ task2/SystemCollections/CompanyData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SystemCollections
+{
+    class CompanyData
+    {
+        public int Number;
+        public int Amount;
+
+        public CompanyData(int num, int amount)
+        {
+            Number = num;
+            Amount = amount;
+        }
+    }
+}
--- task2/SystemCollections/Program.cs
+++ task2/SystemCollections/Program.cs
@@ -15,12 +15,16 @@ namespace SystemCollections
         {
             Hashtable table = new Hashtable();
 
-            table["11"] = 100000;
+            CompanyData data1 = new CompanyData(101, 101151);
+            CompanyData data2 = new CompanyData(102, 101011);
+            CompanyData data3 = new CompanyData(103, 251689);
+            CompanyData data4 = new CompanyData(104, 967811);
 
-            table.Add("kali", 100000);
-            table.Add("debian", 1000000);
-            table.Add("ubuntu", 1000000);
-            table.Add("centos", 1000000);
+            table[data1.Number] = data1.Amount;
+            table[data2.Number] = data2.Amount;
+            table[data3.Number] = data3.Amount;
+            table[data4.Number] = data4.Amount;
+
 
             foreach (DictionaryEntry name in table)
             {
