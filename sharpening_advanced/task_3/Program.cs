cca163f40846e6391cffe7963193cddf42fdb631
Denis Vladimirov <4rx311@gmail.com>
2018-12-10 13:07:11 +0300

    task_4 - regex complete

--- sharpening_advanced/task_3/Program.cs
+++ sharpening_advanced/task_3/Program.cs
@@ -9,19 +9,29 @@ namespace task_3
         static void Main(string[] args)
         {
 
-            MyDirectory dir = new MyDirectory("../../");
-            dir.FilesAmount();
+            //MyDirectory dir = new MyDirectory("../../");
+            //dir.FilesAmount();
 
-            MyFile file = new MyFile(dir.Path, "data.txt");
-            file.FileTextFiller();
-            Console.WriteLine("File initialized...");
-            Console.ReadKey();
+            //MyFile file = new MyFile(dir.Path, "data.txt");
+            //file.FileTextFiller();
+            //Console.WriteLine("File initialized...");
+            //Console.ReadKey();
 
-            dir.FilesAmount();
-            file.PrintFileData();
+            //dir.FilesAmount();
+            //file.PrintFileData();
 
-            Regex regex = new Regex(@"\d{3}-\d{3}-\d{4}");
+            string input = "Отдел документационного обеспечения 8(499) 245-06-12";
+            string pattern = @"\d{1}[(]\d{3}[)] \d{3}-\d{2}-\d{2}";
+            Regex regex = new Regex(pattern);
 
+            MatchCollection matches = regex.Matches(input);
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                    Console.WriteLine(match.Value);
+            }
+
+            Console.WriteLine("End of program...");
             Console.ReadKey();
         }
     }
