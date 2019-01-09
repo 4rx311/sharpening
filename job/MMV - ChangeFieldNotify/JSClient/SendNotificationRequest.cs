ff60318c7889ede97763e6dd638caad6f1c4add9
Denis Vladimirov <4rx311@gmail.com>
2019-01-09 18:00:01 +0300

    job

---
+++ job/MMV - ChangeFieldNotify/JSClient/SendNotificationRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WSSC.V4.DMS.MMV.Controls.ChangeFieldNotify.JSClient
+{
+    /// <summary>
+    /// Структура параметров запроса на отправку уведомления
+    /// </summary>
+    public class SendNotificationRequest
+    {
+        /// <summary>
+        /// Описание измененных полей
+        /// </summary>
+        public List<ChangesDescription> ChangedFields { get; set; }
+        /// <summary>
+        /// Идентификатор списка, в котором находится элемент
+        /// </summary>
+        public int ListID { get; set; }
+        /// <summary>
+        /// Идентификатор элемента
+        /// </summary>
+        public int ItemID { get; set; }
+    }
+}
